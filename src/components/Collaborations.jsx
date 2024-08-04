324c182bb19767a6d842dfeaf5bea6a041129995
purnima <purnimaw3@gmail.com>
2024-08-04 15:47:21 -0400

    collaborations

--- src/components/Collaborations.jsx
+++ src/components/Collaborations.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
 // Import logos
-import ptc from '../assets/ptc.jpg';
+import ptc from '../assets/ptc (1).jpg';
 import nic from '../assets/nic.jpg';
-import devpkr from '../assets/devpkr.jpg';
+import devpkr from '../assets/devpkr (1).jpg';
 import emstreks from '../assets/emstreks.jpg';
 
 const Collaborations = () => {
@@ -14,16 +14,16 @@ const Collaborations = () => {
 
       <div className="flex justify-center flex-wrap">
         <div className="m-2 text-center sm:m-4 lg:m-6">
-          <img src={ptc} alt="Pokhara Tourism Council" className="h-24 mx-auto sm:w-48 lg:w-72" />
+          <img src={ptc} alt="Pokhara Tourism Council" className="h-24 mx-auto sm:w-28 lg:w-32" />
         </div>
         <div className="m-2 text-center sm:m-4 lg:m-6">
-          <img src={nic} alt="NIC" className="h-24 mx-auto sm:w-32 lg:w-48" />
+          <img src={nic} alt="NIC" className="h-24 mx-auto sm:w-32 lg:w-48 lg:ml-8" />
         </div>
         <div className="m-2 text-center sm:m-4 lg:m-6">
-          <img src={devpkr} alt="Developers Community" className="h-24 mx-auto sm:w-32 lg:w-38" />
+          <img src={devpkr} alt="Developers Community" className="h-24 mx-auto sm:w-32 lg:w-38 lg:ml-8" />
         </div>
         <div className="m-2 text-center sm:m-4 lg:m-6">
-          <img src={emstreks} alt="Ems Treks" className="h-24 mx-auto sm:w-32 lg:w-34" />
+          <img src={emstreks} alt="Ems Treks" className="h-24 mx-auto sm:w-32 lg:w-32 lg:ml-8" />
         </div>
       </div>
     </div>
